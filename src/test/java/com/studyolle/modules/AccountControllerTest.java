142487b6fee7d42ccdf1462dddf165e372b4446a
halfdev <gusgodnd@naver.com>
2020-04-05 19:57:24 +0900

    #3 account Junit (Todo, Email Verify)

---
+++ src/test/java/com/studyolle/modules/AccountControllerTest.java
@@ -0,0 +1,73 @@
+package com.studyolle.modules;
+
+import com.studyolle.infra.MockMvcTest;
+import com.studyolle.infra.mail.EmailMessage;
+import com.studyolle.infra.mail.EmailService;
+import com.studyolle.modules.account.Account;
+import com.studyolle.modules.account.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@MockMvcTest
+public class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    AccountRepository accountRepository;
+    @MockBean
+    EmailService emailService;
+
+    @DisplayName("인증 메일 확인 - 입력값 정상")
+    @Test
+    void checkEmailToken() throws Exception {
+        Account account = Account.builder()
+                .email("test@email.com")
+                .password("12345678")
+                .nickname("tester")
+                .build();
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+
+        mockMvc.perform(get("/check-email-token")
+                .param("token", newAccount.getEmailCheckToken())
+                .param("email", newAccount.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("nickname"))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(authenticated().withUsername("tester"));
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 정상")
+    @Test
+    void signUpSubmit_with_correct_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "tester")
+                .param("email", "test@gmail.com")
+                .param("password", "1234")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated().withUsername("tester"));
+
+        Account account = accountRepository.findByEmail("test@gmail.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "1234");
+        assertNotNull(account.getEmailCheckToken());
+        then(emailService).should().sendEmail(any(EmailMessage.class));
+    }
+}
